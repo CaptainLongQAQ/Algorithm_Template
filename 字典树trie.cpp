8e0aa6f1ebba999c635c1487c8f4eefb89b9c229
CaptainLongQAQ <122419074+CaptainLongQAQ@users.noreply.github.com>
2023-09-27 20:20:20 +0800

    Update 字典树trie.cpp

--- 字典树trie.cpp
+++ 字典树trie.cpp
@@ -41,44 +41,45 @@ public:
     }
 };
 
-
-
 //指针写法
 struct node{
     node* next[26];
     int have;
 };
-node* head;
 //初始化每个节点
-void init(node* h){
-    for(int i = 0; i < 26; ++i){
-        h->next[i] = nullptr;
+class Trie{
+private:
+    node* head;
+public:
+    Trie() {
+        head = new node;
+        init(head);
     }
-    h->have = 0;
-}
-//插入字符串
-void insert(string& forbidden){
-    node* p = head;
-    for(int j = 0; j < forbidden.size(); ++j){
-        int u = forbidden[j] - 'a';
-        if(!p->next[u]){
-            p->next[u] = new node; //创建节点
-            init(p->next[u]);
+    void init(node* h){
+        for(int i = 0; i < 26; ++i) h->next[i] = nullptr;
+        h->have = 0;
+    }
+    //插入字符串
+    void insert(string& forbidden){
+        node* p = head;
+        for(int j = 0; j < forbidden.size(); ++j){
+            int u = forbidden[j] - 'a';
+            if(!p->next[u]){
+                p->next[u] = new node; //创建节点
+                init(p->next[u]);
+            }
+            p = p->next[u];
         }
-        p = p->next[u];
+        p->have = 1;
     }
-    p->have = 1;
-}
-//求字符串的个数
-int check(string& s){
-    node* p = head;
-    for(int i = 0; i < s.size(); ++i){
-        int u = s[i] - 'a';
-        if(!p->next[u]){
-            return 0;
+    //求字符串的个数
+    int check(string& s){
+        node* p = head;
+        for(int i = 0; i < s.size(); ++i){
+            int u = s[i] - 'a';
+            if(!p->next[u]) return 0;
+            p = p->next[u];
         }
-        p = p->next[u];
+        return p->have;
     }
-    return p->have;
-}
-
+};
