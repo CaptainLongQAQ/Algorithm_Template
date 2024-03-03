e66bcd50411097d9d9475e1cae5d1a14a674d355
CaptainLongQAQ <122419074+CaptainLongQAQ@users.noreply.github.com>
2024-03-03 19:56:18 +0800

    Add files via upload

---
+++ 树状数组.cpp
@@ -0,0 +1,24 @@
+class BitTree{
+public:
+    vector<int> tree;
+    // 传参 n : 为数组长度+1, 树状数组默认从下标1开始
+    BitTree(int n): tree(n, 0){
+
+    }
+    int low_bit (int x) {return x & -x;}
+    int query (int x) {
+        int res = 0;
+        while (x) {
+            res += tree[x];
+            x -= low_bit(x);
+        }
+        return res;
+    }
+
+    void insert (int x, int k) {
+        while (x < tree.size()) {
+            tree[x] += k;
+            x += low_bit(x);
+        }
+    }
+};
